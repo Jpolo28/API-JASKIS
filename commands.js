905ae154784a659ef6b4b6fa4995090d031b1dc4
Erick Falconi <raidenjr28@gmail.com>
2022-05-11 18:04:27 -0400

    Finished

--- commands.js
+++ commands.js
@@ -84,16 +84,17 @@ db.jaskis.insertMany([{
 // 1. Query for all bounties in the Grasslands
 db.jaskis.find(location="Grasslands")
 // 2. Query for all bounties with a reward worth 10000 or more
-db.jaskis.find(reward= )
+db.jaskis.find(reward: 10000 )
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find({},{ client: 0 })
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find({species: "Groundhog" }, {location: "Woodlands" })
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-
+db.bounties.updateOne({_id: ObjectId('627a0118ccda78ac528f1663')}, {$set: {"reward":"10000"}})
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne({_id: ObjectId('627a0118ccda78ac528f1661')})
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany({ client:"Songbird" })
 // 4. Update all captured statuses to true
+db.bounties.updateMany({ captured: "false"},{$set {captured: "true"} })
